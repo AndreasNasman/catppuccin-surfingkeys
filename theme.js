9d7449c56474210f804c1c5597ff632c58a39280
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-07-02 11:28:41 +0300

    Update stylings to Catppuccin theme

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 /* Unmap the Ace Vim editor. */
-api.unmap("<Ctrl-i>");
 api.iunmap("<Ctrl-i>");
+api.unmap("<Ctrl-i>");
 
 /* Unmap emoji completion in insert mode. */
 api.iunmap(":");
@@ -13,88 +13,126 @@ settings.tabsThreshold = 0;
 
 settings.theme = `
 :root {
-  --dracula-background: #282a36;
-  --dracula-comment: #6272a4;
-  --dracula-cyan: #8be9fd;
-  --dracula-foreground: #f8f8f2;
-  --dracula-green: #50fa7b;
-  --dracula-orange: #ffb86c;
-  --dracula-pink: #ff79c6;
-  --dracula-purple: #bd93f9;
-  --dracula-selection: #44475a;
-  --dracula-yellow: #f1fa8c;
+  --catppuccin-base: #1e1e2e;
+  --catppuccin-blue: #89b4fa;
+  --catppuccin-crust: #11111b;
+  --catppuccin-flamingo: #f2cdcd;
+  --catppuccin-green: #a6e3a1;
+  --catppuccin-lavender: #b4befe;
+  --catppuccin-mantle: #181825;
+  --catppuccin-maroon: #eba0ac;
+  --catppuccin-mauve: #cba6f7;
+  --catppuccin-overlay0: #6c7086;
+  --catppuccin-overlay1: #7f849c;
+  --catppuccin-overlay2: #9399b2;
+  --catppuccin-peach: #fab387;
+  --catppuccin-pink: #f5c2e7;
+  --catppuccin-red: #f38ba8;
+  --catppuccin-rosewater: #f5e0dc;
+  --catppuccin-sapphire: #74c7ec;
+  --catppuccin-sky: #89dceb;
+  --catppuccin-subtext0: #a6adc8;
+  --catppuccin-subtext1: #bac2de;
+  --catppuccin-surface0: #313244;
+  --catppuccin-surface1: #45475a;
+  --catppuccin-surface2: #585b70;
+  --catppuccin-teal: #94e2d5;
+  --catppuccin-text: #cdd6f4;
+  --catppuccin-yellow: #f9e2af;
 }
 
+/*
+  =============
+  Bookmarks (b)
+  =============
+*/
+
 .sk_theme {
-  background-color: var(--dracula-background);
-  color: var(--dracula-foreground);
+  background: var(--catppuccin-base);
+  color: var(--catppuccin-text);
   font-family: "JetBrains Mono";
+}
+
+.sk_theme .prompt,
+.sk_theme .separator,
+.sk_theme .resultPage {
+  color: var(--catppuccin-yellow);
 }
 
 .sk_theme input {
   color: inherit;
 }
 
+/* Disable alternating colors */
+.sk_theme #sk_omnibarSearchResult > ul > li:nth-child(odd) {
+  background: inherit;
+}
+
+.sk_theme #sk_omnibarSearchResult > ul > li.focused {
+  background-color: var(--catppuccin-surface0);
+}
+
+.sk_theme .omnibar_highlight {
+  color: var(--catppuccin-green);
+}
+
+.sk_theme .omnibar_folder {
+  color: var(--catppuccin-mauve);
+}
+
+.sk_theme .omnibar_timestamp {
+  color: var(--catppuccin-maroon);
+}
+
 .sk_theme .url {
-  color: var(--dracula-cyan);
+  color: var(--catppuccin-sky);
+}
+
+/*
+  =======
+  URL (t)
+  =======
+*/
+
+.sk_theme .omnibar_visitcount {
+  color: var(--catppuccin-peach);
+}
+
+/*
+  ========
+  Help (?)
+  ========
+*/
+
+.sk_theme .feature_name {
+  color: var(--catppuccin-peach);
+}
+
+.sk_theme kbd {
+  background-color: var(--catppuccin-mantle);
+  color: inherit;
 }
 
 .sk_theme .annotation {
   color: inherit;
 }
 
-.sk_theme kbd {
-  color: var(--dracula-foreground);
-  background-color: inherit;
-}
+/*
+  ==============
+  Rich hints (;)
+  ==============
+*/
 
-.sk_theme .omnibar_highlight {
-  color: var(--dracula-green);
-}
-
-.sk_theme .omnibar_folder {
-  color: var(--dracula-purple);
-}
-
-.sk_theme .omnibar_timestamp {
-  color: var(--dracula-pink);
-}
-
-.sk_theme .omnibar_visitcount {
-  color: var(--dracula-orange);
-}
-
-.sk_theme .prompt,
-.sk_theme .resultPage {
-  color: var(--dracula-yellow);
-}
-
-.sk_theme .feature_name {
-  color: var(--dracula-orange);
-}
-
-.sk_theme .separator {
-  color: var(--dracula-yellow);
-}
-
-.sk_theme #sk_omnibarSearchResult > ul > li:nth-child(odd) {
-  background-color: inherit;
-}
-
-.sk_theme #sk_omnibarSearchResult > ul > li.focused {
-  background-color: var(--dracula-selection);
-}
-
-.expandRichHints span.annotation {
-  color: var(--dracula-comment);
+#sk_keystroke {
+  background-color: var(--catppuccin-base);
+  color: var(--catppuccin-pink);
 }
 
 .expandRichHints kbd > .candidates {
-  color: var(--dracula-green);
+  color: var(--catppuccin-pink);
 }
 
-#sk_keystroke {
-  background-color: var(--dracula-background);
-  color: var(--dracula-green);
+.expandRichHints span.annotation {
+  color: var(--catppuccin-lavender);
 }
 `;
