2abf5294e196dbbd0912a490b862cf7caf0ca430
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-08-12 15:20:27 +0300

    Format document with Biome

--- theme.js
+++ theme.js
@@ -9,32 +9,32 @@ api.iunmap(":");
 api.Hints.setCharacters("fdsajklghvcxzmbnuioprewqty");
 
 const Catppuccin = {
-  Base: "#1e1e2e",
-  Blue: "#89b4fa",
-  Crust: "#11111b",
-  Flamingo: "#f2cdcd",
-  Green: "#a6e3a1",
-  Lavender: "#b4befe",
-  Mantle: "#181825",
-  Maroon: "#eba0ac",
-  Mauve: "#cba6f7",
-  Overlay0: "#6c7086",
-  Overlay1: "#7f849c",
-  Overlay2: "#9399b2",
-  Peach: "#fab387",
-  Pink: "#f5c2e7",
-  Red: "#f38ba8",
-  Rosewater: "#f5e0dc",
-  Sapphire: "#74c7ec",
-  Sky: "#89dceb",
-  Subtext0: "#a6adc8",
-  Subtext1: "#bac2de",
-  Surface0: "#313244",
-  Surface1: "#45475a",
-  Surface2: "#585b70",
-  Teal: "#94e2d5",
-  Text: "#cdd6f4",
-  Yellow: "#f9e2af",
+	Base: "#1e1e2e",
+	Blue: "#89b4fa",
+	Crust: "#11111b",
+	Flamingo: "#f2cdcd",
+	Green: "#a6e3a1",
+	Lavender: "#b4befe",
+	Mantle: "#181825",
+	Maroon: "#eba0ac",
+	Mauve: "#cba6f7",
+	Overlay0: "#6c7086",
+	Overlay1: "#7f849c",
+	Overlay2: "#9399b2",
+	Peach: "#fab387",
+	Pink: "#f5c2e7",
+	Red: "#f38ba8",
+	Rosewater: "#f5e0dc",
+	Sapphire: "#74c7ec",
+	Sky: "#89dceb",
+	Subtext0: "#a6adc8",
+	Subtext1: "#bac2de",
+	Surface0: "#313244",
+	Surface1: "#45475a",
+	Surface2: "#585b70",
+	Teal: "#94e2d5",
+	Text: "#cdd6f4",
+	Yellow: "#f9e2af",
 };
 
 // https://github.com/brookhong/Surfingkeys/blob/master/docs/API.md#visualstyle
@@ -52,9 +52,9 @@ const styles = `
 `;
 api.Hints.style(styles);
 api.Hints.style(
-  `div { ${styles} }
+	`div { ${styles} }
    div.begin { color: ${Catppuccin.Crust}; }`,
-  "text",
+	"text",
 );
 
 /* Always use the omnibar when switching tabs. */
@@ -63,8 +63,8 @@ settings.tabsThreshold = 0;
 settings.theme = `
 :root {
   ${Object.entries(Catppuccin)
-    .map(([key, value]) => `--catppuccin-${key.toLowerCase()}: ${value};`)
-    .join(" ")}
+		.map(([key, value]) => `--catppuccin-${key.toLowerCase()}: ${value};`)
+		.join(" ")}
 }
 
 /*
